dcee3d68dfa7af6c5f8944603e4ef461caf514c7
Nightt_Coderrss <uma102112@gmail.com>
2024-09-19 23:20:04 +0530

    second-commit

---
+++ Maths/Factorial_Of_Number.java
@@ -0,0 +1,18 @@
+/** u can give n below 100 */
+import java.util.Scanner;
+class Factorial_Of_Number {
+    public static double factorial(int n){
+        double res =1;
+            for(int i=n;i>=1;i--){
+                res *= i;
+            }
+            return res;
+    }
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.printf("%.0f",factorial(n));
+        sc.close();
+        
+    }
+}
